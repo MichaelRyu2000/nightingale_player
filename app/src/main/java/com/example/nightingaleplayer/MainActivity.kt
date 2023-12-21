38cea4cb158da2555af768cda3ec895373dbadf2
MichaelRyu2000 <ryumichael2000@gmail.com>
2023-12-20 16:07:08 -0800

    Added basic audio functionality. Ability to play a specific song (must be labeled as audio.mp3) placed into a res/raw folder, along with the ability to pause said song

--- app/src/main/java/com/example/nightingaleplayer/MainActivity.kt
+++ app/src/main/java/com/example/nightingaleplayer/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.nightingaleplayer
 
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.nightingaleplayer.ui.theme.NightingalePlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +41,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PlayerScreen()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PlayerScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { TopAppBar(
+            title = { Text(text = stringResource(R.string.app_name)) }
+        ) },
+        content = { AudioPlayer(modifier = Modifier.fillMaxSize()) }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NightingalePlayerTheme {
-        Greeting("Android")
+fun AudioPlayer(modifier: Modifier = Modifier) {
+    val musContext = LocalContext.current
+
+    val player = MediaPlayer.create(musContext, R.raw.audio)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        Row {
+            IconButton(
+                onClick = { player.start() },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_play_circle_outline_24),
+                    contentDescription = null
+                )
+            }
+            IconButton(
+                onClick = { player.pause() },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_pause_circle_outline_24),
+                    contentDescription = null
+                )
+            }
+        }
     }
 }
